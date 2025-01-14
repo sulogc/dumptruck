1ac834e461e45eafe3d32586a52e2ea818af01df
root <root@localhost.localdomain>
2025-01-14 16:35:28 +0900

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sulogc/dumptruck
 
 go 1.22.6
+
+require github.com/confluentinc/confluent-kafka-go/v2 v2.8.0
--- main.go
+++ main.go
@@ -1,5 +1,69 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"time"
+
+	// "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <bootstrap-server> <topic>\n", os.Args[0])
+		os.Exit(1)
+	}
+	bootstrapServers := os.Args[1]
+	topic := os.Args[2]
+	totalMsgcnt := 3
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"boostrap.servers": bootstrapServers})
+	if err != nil {
+		fmt.Printf("Failed to create producer: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Created Producer %v\n", p)
+
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				m := ev
+				if m.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
+				} else {
+					fmt.Printf("Delivery message to topic %s [%d] at offset %v \n",
+						*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+				}
+			case kafka.Error:
+				fmt.Printf("Error: %v\n", ev)
+			default:
+				fmt.Printf("Ignored event: %s\n", ev)
+			}
+		}
+	}()
+	msgcnt := 0
+	for msgcnt < totalMsgcnt {
+		value := fmt.Sprintf("Producer example, message #%d", msgcnt)
+		err = p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(value),
+			Headers:        []kafka.Header{{Key: "myTestHeader", Value: []byte("header values are binary")}},
+		}, nil)
+
+		if err != nil {
+			if err.(kafka.Error).Code() == kafka.ErrQueueFull {
+				time.Sleep(time.Second)
+				continue
+			}
+			fmt.Printf("Failed to produce message: %v\n", err)
+		}
+		msgcnt++
+	}
+	for p.Flush(10000) > 0 {
+		fmt.Print("Still waiting to flush outstanding messages\n")
+	}
+	p.Close()
 
 }
